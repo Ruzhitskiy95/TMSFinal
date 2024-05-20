984890560fd88c39cb96486e566c0eeb4060141f
Ruzhitskiy95 <th1463975@yandex.ru>
2024-05-20 22:58:53 +0300

    First test commit

--- src/main/java/com/example/tmsfinal/TmsFinalApplication.java
+++ src/main/java/com/example/tmsfinal/TmsFinalApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TmsFinalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TmsFinalApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TmsFinalApplication.class, args);
+    }
 }
