c62b8f070bee7e443ee953657814b618e48a1697
Ruzhitskiy95 <th1463975@yandex.ru>
2024-05-20 22:45:40 +0300

    Initial commit

---
+++ src/main/java/com/example/tmsfinal/TmsFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.tmsfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmsFinalApplication.class, args);
+	}
+
+}
